a2fbecf04a36b39be169bb184170d7f6b35e99e9
YasykurKhalis <yaskurkaka@gmail.com>
2025-03-15 23:59:29 +0700

    soal 2

---
+++ soal_2/scripts/frag_monitor.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Ambil penggunaan RAM dalam persentase
+ram_usage=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
+
+# Ambil total dan available RAM
+total_ram=$(free -h | grep Mem | awk '{print $2}')
+available_ram=$(free -h | grep Mem | awk '{print $7}')
+
+# Log ke file
+log_dir="$(dirname "$0")/../logs"
+log_file="$log_dir/core.log"
+
+mkdir -p "$log_dir"
+
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] - Fragment Usage [$ram_usage%] - Fragment Count [$total_ram] - Details [Total: $total_ram, Available: $available_ram]" >> $log_file
