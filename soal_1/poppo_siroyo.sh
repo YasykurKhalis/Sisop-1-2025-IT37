37dd779b3fc0a276b9411f88dcb28653298332a9
YasykurKhalis <yaskurkaka@gmail.com>
2025-03-15 18:17:08 +0700

    soal 1

---
+++ soal_1/poppo_siroyo.sh
@@ -0,0 +1,47 @@
+awk -F ',' '
+{ count[$2]++ } 
+END {
+    if (count["Chris Hemsworth"] > 0) {
+        print "Chris Hemsworth membaca", count["Chris Hemsworth"], "buku."
+    } else {
+        print "Chris Hemsworth tidak ditemukan dalam data."
+    }
+}' reading_data.csv
+
+awk -F ',' '
+{
+    count[$8]++;
+    total[$8] = total[$8] + $6
+}
+END {
+    if (count["Tablet"] > 0) {
+        print "Rata-rata durasi membaca dengan Tablet adalah", total["Tablet"] / count["Tablet"], "menit.";
+    } else {
+        print "Tidak ada data membaca dengan Tablet.";
+    }
+}' reading_data.csv
+
+awk -F ',' '
+NR > 1 { 
+    if ($7 > max_rating) { 
+        max_rating = $7;
+        name = $2;
+        book = $3;
+    } 
+} 
+END { 
+    print "Pembaca dengan rating tertinggi:", name, "-", book, "-", max_rating;
+}' reading_data.csv
+
+awk -F ',' 'NR > 1 && $9 == "Asia" && $5 > "2023-12-31" {genre_count[$4]++}
+END {
+    max_genre = ""
+    max_count = 0
+    for (genre in genre_count) {
+        if (genre_count[genre] > max_count) {
+            max_count = genre_count[genre]
+            max_genre = genre
+        }
+    }
+    print "Genre paling populer di Asia setelah 2023 adalah", max_genre, "dengan", max_count, "buku."
+}' reading_data.csv
