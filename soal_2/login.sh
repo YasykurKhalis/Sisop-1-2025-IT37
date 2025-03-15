3484882596ed6d23a691123c5b21930062689acc
YasykurKhalis <yaskurkaka@gmail.com>
2025-03-15 23:08:34 +0700

    soal 2

---
+++ soal_2/login.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+path="./data/player.csv"
+
+read -p "Masukkan email: " email
+read -s -p "Masukkan password: " password
+
+hashed_password=$(echo "$password" | sha256sum | awk '{print $1}')
+
+if grep -q "^$email,.*,$hashed_password$" "$path"; then
+ echo "Berhasil login"
+ exit 0
+else
+ echo "Gagal login"
+ exit 1
+fi
---
+++ soal_2/scripts/core_monitor.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Ambil persentase penggunaan CPU
+cpu_usage=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}')
+
+# Ambil model CPU
+cpu_model=$(lscpu | grep "Model name" | cut -d ':' -f 2 | xargs)
+
+# Log ke file
+log_file="../logs/core.log"
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] - Core Usage [$cpu_usage%] - Terminal Model [$cpu_model]" >> $log_file
